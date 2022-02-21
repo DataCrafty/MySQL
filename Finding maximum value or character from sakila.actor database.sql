847b855b14b6f8f1596393f121b91644f0983bea
Chandrakala B <99250706+DataCrafty@users.noreply.github.com>
2022-02-21 17:44:57 +0530

    Add files via upload

---
+++ Finding maximum value or character from sakila.actor database.sql
@@ -0,0 +1,5 @@
+select * from sakila.actor;
+select MAX(actor_id) as minimum_id from sakila.actor;
+select MAX(first_name) as highest_name from sakila.actor;
+select max(last_name) as highest_name from sakila.actor;
+select max(last_update) as highest_update from sakila.actor;
