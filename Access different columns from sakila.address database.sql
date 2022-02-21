4d663adce33550ade4020f70276c48d601ef738a
Chandrakala B <99250706+DataCrafty@users.noreply.github.com>
2022-02-21 17:47:32 +0530

    Add files via upload

---
+++ Access different columns from sakila.address database.sql
@@ -0,0 +1,6 @@
+select * from sakila.address;
+select address, district from sakila.address;
+select last_update from sakila.address;
+select phone,postal_code from sakila.address;
+select district from sakila.address;
+select location from sakila.address;
