cfdc0ac29b57747ef1b6950ea665ed50bff090fb
Chandrakala B <99250706+DataCrafty@users.noreply.github.com>
2022-02-21 17:48:14 +0530

    Add files via upload

---
+++ Averages of all columns of sakila.payments database.sql
@@ -0,0 +1,6 @@
+select * from sakila.payment;
+select AVG(rental_id) from sakila.payment;
+select AVG(customer_id) from sakila.payment;
+select AVG(staff_id) from sakila.payment;
+select AVG(amount) from sakila.payment;
+select AVG(payment_id) from sakila.payment;
