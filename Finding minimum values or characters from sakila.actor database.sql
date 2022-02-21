ca4a8a810c28788f0664ef8919d6dee293861a5d
Chandrakala B <99250706+DataCrafty@users.noreply.github.com>
2022-02-21 17:56:33 +0530

    Add files via upload

---
+++ Finding minimum values or characters from sakila.actor database.sql
@@ -0,0 +1,5 @@
+select * from sakila.actor;
+select MIN(actor_id) as minimum_id from sakila.actor;
+select MIN(first_name) as first_name from sakila.actor;
+select MIN(last_name) as last_name from sakila.actor;
+select MIN(last_update) as last_update from sakila.actor;
