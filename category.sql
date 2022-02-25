d5271f9de3f5ae57f8010df8f14206c1184832cb
Chandrakala B <99250706+DataCrafty@users.noreply.github.com>
2022-02-26 01:05:07 +0530

    Add files via upload
    The queries about catogory

---
+++ category.sql
@@ -0,0 +1,7 @@
+select *from sakila.category;
+select name from sakila.category;
+select last_update from sakila.category;
+select category_id from sakila.category;
+select name,last_update,category_id from sakila.category;
+select *from sakila.category where category_id=5 and category_id=4 OR category_id=4 ;
+
