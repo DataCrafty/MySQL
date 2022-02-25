b7d4257ce48bbb36127140a745884828d0c58140
Chandrakala B <99250706+DataCrafty@users.noreply.github.com>
2022-02-25 23:50:03 +0530

    Add files via upload
    The customer details are provided  in the MySQL querries

---
+++ conditions used in sakila.inventory database.py
@@ -0,0 +1,6 @@
+select * from sakila.language;
+select language_id from sakila.language;
+select name from sakila.language;
+select last_update from sakila.language;
+select language_id=1 from sakila.language; 
+select name='English' from sakila.language;
