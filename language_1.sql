aafe7b131e2dbd54ee1e172af69288b380bab9a7
Chandrakala B <99250706+DataCrafty@users.noreply.github.com>
2022-02-26 00:04:03 +0530

    Add files via upload
    The MySQL querries about language

---
+++ language_1.sql
@@ -0,0 +1,7 @@
+select *from sakila.language;
+select language_id from sakila.language;
+select name from sakila.language;
+select last_update,language_id,name from sakila.language;
+select last_update,name from sakila.language;
+select language_id,last_update from sakila.language;
+select  language_id,name  from sakila.language;
